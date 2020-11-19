67ea72339d9035525a23d837796e482c8bde91e2
Sailesh Ashokbhai Patel <s.patel@kainos.com>
2020-11-19 15:25:14 +0000

    add SQL code for SalesEmployee

--- SQL/start.sql
+++ SQL/start.sql
@@ -15,3 +15,12 @@ CREATE TABLE `Employee` (
   `BIC` varchar(11),
   PRIMARY KEY (`EmployeeID`)
 );
+
+CREATE TABLE `SalesEmployee` (
+  `SalesEmployeeID` int NOT NULL AUTO_INCREMENT,
+  `EmployeeID` int,
+  `CommissionRate` decimal(3,2) NOT NULL,
+  `SalesTotal` decimal(10, 2) NOT NULL,
+  PRIMARY KEY (`SalesEmployeeID`),
+  FOREIGN KEY (`EmployeeID`) REFERENCES `Employee`(`EmployeeID`)
+)
